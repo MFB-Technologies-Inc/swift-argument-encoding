15a32c300634fd08433e9e958d5e4c7c6191599d
Andrew Roan <roanutil@users.noreply.github.com>
2023-05-23 23:00:24 -0500

    Add OptionDecodingTests and OptionSetDecodingTests
    feature/improve-decodable-configuration

---
+++ Tests/ArgumentEncodingTests/OptionSetDecodingTests.swift
@@ -0,0 +1,40 @@
+// OptionSetDecodingTests.swift
+// ArgumentEncoding
+//
+// Copyright © 2023 MFB Technologies, Inc. All rights reserved.
+
+import ArgumentEncoding
+import Foundation
+import XCTest
+
+final class OptionSetDecodingTests: XCTestCase {
+    let encoder = JSONEncoder()
+
+    func testDecodeOptionSet() throws {
+        let optionSet = OptionSet(wrappedValue: ["value1", "value2"])
+        let data = try encoder.encode(optionSet)
+        let decoder = JSONDecoder()
+        let configurationKey = try XCTUnwrap(OptionSet<[String]>.configurationCodingUserInfoKey())
+        decoder.userInfo[configurationKey] = OptionSet<[String]>.unwrap
+        let decoded = try decoder.decode(OptionSet<[String]>.self, from: data)
+        XCTAssertEqual(decoded, optionSet)
+    }
+
+    func testDecodeOptionSetContainer() throws {
+        let container = OptionSetContainer(option: ["value1", "value2"])
+        let data = try encoder.encode(container)
+        let decoder = JSONDecoder()
+        let configurationKey = try XCTUnwrap(OptionSet<[String]>.configurationCodingUserInfoKey())
+        decoder.userInfo[configurationKey] = Option<String>.unwrap
+        let decoded = try decoder.decode(OptionSetContainer.self, from: data)
+        XCTAssertEqual(decoded, container)
+    }
+}
+
+private struct OptionSetContainer: ArgumentGroup, Codable, Equatable {
+    @OptionSet var option: [String]
+
+    init(option: [String]) {
+        _option = OptionSet(wrappedValue: option)
+    }
+}
