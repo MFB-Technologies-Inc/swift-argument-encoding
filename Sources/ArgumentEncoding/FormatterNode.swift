b209b0c81d9d6f369ab22512ad5cf05901fcc4ce
Andrew Roan <roanutil@users.noreply.github.com>
2025-04-18 15:13:49 -0500

    Update file header
    feature/update-workflow

--- Sources/ArgumentEncoding/FormatterNode.swift
+++ Sources/ArgumentEncoding/FormatterNode.swift
@@ -1,9 +1,7 @@
 // FormatterNode.swift
 // ArgumentEncoding
 //
-// Copyright © 2024 MFB Technologies, Inc. All rights reserved.
-//
-// This source code is licensed under the MIT license found in the
+// This source code is licensed under the MIT License (MIT) found in the
 // LICENSE file in the root directory of this source tree.
 
 /// A protocol that describes a type that defines how flags and options are formatted. Typically, `ArgumentGroup`
